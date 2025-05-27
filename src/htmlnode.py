de53b6e3cac7dff78148a6f02e34ef540f1e2b37
hallafax <vswaze@gmail.com>
2025-05-26 21:39:57 -0400

    added leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,4 +17,25 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
-        return functools.reduce(lambda a: " " + str(a.keys()) + str(a.values()), self.props)
+        prop_string = ""
+        for key in self.props:
+            prop_string += f' {key}="{self.props[key]}"'
+        return prop_string
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, children=None, props=None):
+        super().__init__(tag,value,None,props)
+        if children!=None:
+            raise Exception("cannot have children")
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        elif self.tag == None:
+            return str(self.value)
+        elif self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        else:    
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,4 +1,4 @@
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 import unittest
 
 class TestTextNode(unittest.TestCase):
@@ -11,6 +11,20 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node3, node4)
         self.assertNotEqual(node, node3)
         self.assertNotEqual(node2, node4)
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        node2 = LeafNode("a", "Click me!", None, {"href": "https://www.google.com", })
+        node3 = LeafNode("a", "Click me!", None, {"href": "https://www.google.com", "color": "red" })
+        node4 = LeafNode("b", None)
+
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        self.assertEqual(node2.to_html(), '<a href="https://www.google.com">Click me!</a>')
+        self.assertEqual(node3.to_html(), '<a href="https://www.google.com" color="red">Click me!</a>')
+        with self.assertRaises(ValueError):
+            node4.to_html()
+        with self.assertRaises(Exception):
+            node5= LeafNode("p", "this is hard", "b", {"color": "red"})
 
 
 if __name__ == "__main__":
